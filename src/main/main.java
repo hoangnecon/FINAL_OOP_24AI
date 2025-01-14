00acc03905b27600439d3d71f4e00d676d657b3d
NMNBen <nmnben0409@gmail.com>
2025-01-14 14:12:47 +0700

    Fitting scale for monitor

--- src/main/main.java
+++ src/main/main.java
@@ -1,8 +1,12 @@
 package main;
 import javax.swing.*;
 public  class main extends JFrame {
+
     public static void main(String[] args) {
-        new LoginMain();
+        SwingUtilities.invokeLater(() -> {
+            System.setProperty("sun.java2d.uiScale", "1.0");
+            new LoginMain();
+        });
     }
 }
 
