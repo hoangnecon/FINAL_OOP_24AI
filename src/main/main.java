1a969e979ce03f233b083273034ad251d1f028b0
NMNBen <nmnben0409@gmail.com>
2025-01-14 14:26:07 +0700

    test

--- src/main/main.java
+++ src/main/main.java
@@ -3,10 +3,10 @@ import javax.swing.*;
 public  class main extends JFrame {
 
     public static void main(String[] args) {
-        SwingUtilities.invokeLater(() -> {
-            System.setProperty("sun.java2d.uiScale", "1.0");
+//        SwingUtilities.invokeLater(() -> {
+//            System.setProperty("sun.java2d.uiScale", "1.0");
             new LoginMain();
-        });
+//        });
     }
 }
 
